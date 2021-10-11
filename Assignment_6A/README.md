3b551181cec8874837db4047d24b3d324acb42a5
Lily Ting <lting@andrew.cmu.edu>
2021-10-11 17:40:38 -0400

    copied all files from assignment 5 into assignment 6 folder

---
+++ Assignment_6A/README.md
@@ -0,0 +1,5 @@
+# Assignment5
+PUI Assignment 5
+
+Extra Credit:
+I created a Contact page in addition to the 3 required screens. I utilized label and input tags to create an email form.
---
+++ Assignment_6A/js/Assignment5_js.js
@@ -0,0 +1,35 @@
+// Overlay effect
+function on() {
+  document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+  document.getElementById("overlay").style.display = "none";
+}
+
+
+
+
+
+
+
+//Dropdown Button
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.drop-btn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}

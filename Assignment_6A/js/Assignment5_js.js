e9168a30c3b2e7a5fcaa6c1096e0819ab756b3d6
Lily Ting <lting@andrew.cmu.edu>
2021-10-22 09:09:01 -0400

    updated product details page again

--- Assignment_6A/js/Assignment5_js.js
+++ Assignment_6A/js/Assignment5_js.js
@@ -44,16 +44,22 @@ function addToCart() {
     return;
   }
 
+  //cartItems is a JSON array. 
+  //cartItemsStr is a json array string, or is null
+  //cartItems is null if cartItemsStr is null. So then we initialize it
+
+
   // get local stored cart item array as a string
   let cartItemsStr = localStorage.getItem('cartItems')
   // convert to json object
-  var cartItems = JSON.parse(cartItemsStr);
+  var cartItems = JSON.parse(cartItemsStr); // Turns a string into code - creates an “object” - an instance of a JSON class. Need a JSON class object in order to add onto it
   // if no local stored cart item array, initialize to empty array
   if (cartItems == null) {
     cartItems = JSON.parse('[]');
   }
   
   // construct array data of form selections
+  //The FormData part creates an array and the for loop turns it into a JSON object. CartItems is an array of JSON
   const newItem = Array.from(new FormData(form));
 
   // creating empty object to convert array format
